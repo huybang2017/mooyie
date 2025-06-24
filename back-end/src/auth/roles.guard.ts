015a73cad19ac43b7b2bb34f4b06bd0870f9e1c7
huybang2017 <ndhbang2017@gmail.com>
2025-06-24 19:25:10 +0700

    feat: add guard role

---
+++ back-end/src/auth/roles.guard.ts
@@ -0,0 +1,34 @@
+// src/common/guards/roles.guard.ts
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'generated/prisma';
+import { ROLES_KEY } from 'src/auth/decorate/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Bạn không có quyền truy cập');
+    }
+
+    return true;
+  }
+}
